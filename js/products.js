8d7892dbf236a1afd53aba51742770f8e558a997
Dmitriy <boltthrower@mail.ru>
2022-09-28 19:12:13 +0300

    Готово

---
+++ js/products.js
@@ -0,0 +1,22 @@
+Vue.component('products', {
+    props:['products',],
+    template: 
+                `<div class="products">
+                    <product v-for="item of products" 
+                    :key="item.id_product"
+                    :product="item"></product>
+                </div>`
+});
+
+Vue.component('product', {
+    props:['product',],
+    template:
+        `<div class="product-item">
+            <img v-bind:src="'img/' + product.id_product + '.jpg'" alt="Some img">
+            <div class="desc">
+                <h3>{{product.product_name}}</h3>
+                <p>{{product.price}}</p>
+                <button class="buy-btn" @click="$parent.$emit('add-product', product)">Купить</button>
+            </div>
+        </div>`
+})
